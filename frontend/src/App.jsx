bd24347af3465c63d83af7e323479b89cec8e7b6
BT-0001 <nidhiranjanjaiswal2021@gmail.com>
2026-04-21 11:44:33 +0530

    connecting backend and frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,7 +16,7 @@ export default function App() {
     setError("")
 
     try {
-      const response = await fetch("http://127.0.0.1:8000/analyze", {
+      const response = await fetch("https://nidhi1531-llm-deveops-backend.hf.space/analyze", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ log }),
